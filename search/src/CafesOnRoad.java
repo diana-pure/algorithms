56654a68d8509441ecafee96d0f1e97191d395df
diana-pure <diana.anisutina@gmail.com>
2026-02-15 17:21:43 +0400

    STDY-22 check max and min cafes possible and needed to place

--- search/src/CafesOnRoad.java
+++ search/src/CafesOnRoad.java
@@ -13,40 +13,62 @@ public class CafesOnRoad {
             for (int i = 0; i < l; i++) {
                 cafes[i] = scanner.nextInt();
             }
-            int dd = f(n, k, m, l, cafes);
-            if (dd <= m) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
+            f(n, k, m, l, cafes);
         }
     }
 
-    public static int f(int n, int k, int m, int l, int[] cafes) {
+    public static void f(int n, int k, int m, int l, int[] cafesBetween) {
+        int[] cafes = Arrays.copyOf(cafesBetween, cafesBetween.length + 2);
+        cafes[cafesBetween.length] = 0;
+        cafes[cafesBetween.length + 1] = n;
         Arrays.sort(cafes);
-        int[] distances = new int[l + 1]; // distances
+        int[] distances = new int[cafes.length - 1]; // distances
         int i;
         for (i = 0; i < cafes.length - 1; i++) {
             distances[i] = cafes[i + 1] - cafes[i];
         }
-        distances[cafes.length - 1] = cafes[0];
-        distances[cafes.length] = n - cafes[cafes.length - 1];
         Arrays.sort(distances);
 
-        int low = 0;
-        int high = l;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (m > distances[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int cnt = 0; // minimum needed to cover at least m distance cafes
+        for (int j = distances.length - 1; j >= 0; j--) {
+            if (distances[j] < m) {
+                break;
+            }
+            int r = distances[j];
+            cnt += r % m == 0 ? r / m - 1 : r / m;
+        }
+
+        int cnt2 = 0; // maximum slots available to place a cafe
+        for (i = 0; i < cafes.length - 1; i++) {
+            if (distances[i] > 2) {
+                cnt2 += distances[i] - 2;
             }
         }
-        int spare = 0;
-        for (int j = low - 1; j <= distances.length - 1; j++) {
-            spare += distances[j];
+
+        if (cnt <= k && k <= cnt2) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
-        return spare / (k + 1);
+
+//        int low = 0;
+//        int high = distances.length - 1;
+//        int mid;
+//        while (low <= high) {
+//            mid = low + (high - low) / 2;
+//            if (m > distances[mid]) {
+//                high = mid - 1;
+//            } else {
+//                low = mid + 1;
+//            }
+//        }
+//        int spare = 0;
+//        if (low - 1 < 0 || low - 1 > distances.length - 1) {
+//            return m + 1;
+//        }
+//        for (int j = low - 1; j <= distances.length - 1; j++) {
+//            spare += distances[j];
+//        }
+//        return spare / (k + 1);
     }
 }
--- search/tests/CafeOnRoadTest.java
+++ search/tests/CafeOnRoadTest.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class CafeOnRoadTest {
 
     public static void main(String[] args) {
-        test1();
+        test2();
     }
 
     public static void test1() {
@@ -14,12 +14,44 @@ public class CafeOnRoadTest {
         int l = 3;
         int[] cafes = new int[]{6, 3, 12};
         long start = System.currentTimeMillis();
-        int dd = CafesOnRoad.f(n, k, m, l, cafes);
-        System.out.println("time: " + (System.currentTimeMillis() - start) + "ms");
-        if (dd <= m) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
+//        int dd = CafesOnRoad.f(n, k, m, l, cafes);
+//        System.out.println("time: " + (System.currentTimeMillis() - start) + "ms");
+//        if (dd <= m) {
+//            System.out.println("YES");
+//        } else {
+//            System.out.println("NO");
+//        }
+    }
+
+    public static void test2() {
+        int n = 15;
+        int k = 2;
+        int m = 1;
+        int l = 3;
+        int[] cafes = new int[]{6, 3, 12};
+        long start = System.currentTimeMillis();
+//        int dd = CafesOnRoad.f(n, k, m, l, cafes);
+//        System.out.println("time: " + (System.currentTimeMillis() - start) + "ms");
+//        if (dd <= m) {
+//            System.out.println("YES");
+//        } else {
+//            System.out.println("NO");
+//        }
+    }
+
+    public static void test3() {
+        int n = 15;
+        int k = 2;
+        int m = 3;
+        int l = 0;
+        int[] cafes = new int[]{};
+        long start = System.currentTimeMillis();
+//        int dd = CafesOnRoad.f(n, k, m, l, cafes);
+//        System.out.println("time: " + (System.currentTimeMillis() - start) + "ms");
+//        if (dd <= m) {
+//            System.out.println("YES");
+//        } else {
+//            System.out.println("NO");
+//        }
     }
 }
